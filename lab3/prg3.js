94535d9086113705fb650780223bfe5d4e8fa71b
akash0451 <akashakg05@gmail.com>
2026-08-21 15:41:34 +0530

    lab3 initial commit

--- lab3/prg3.js
+++ lab3/prg3.js
@@ -4,7 +4,7 @@ const server =http.createServer((req,res) => {
     console.log('Server hit by client');
     // res.write("<h1>Hello Client</h1>");
     res.writeHead(200,{"content-type" :"text/html"});
-    res.end("<h2>Hello Client</h2>");
+    res.end("<h2>Hello client</h2>");
 });
 
 server.listen(4444, () => console.log("Server is running..."));
